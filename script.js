5c70d35f577f42dc1fb1cc764749d3f5c026afaf
ariran <ari.t.rantanen@gmail.com>
2025-06-01 20:50:33 +0000

    async-yritelma

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const MIN_LENGTH = 4;
 var lettersCount = 0;
 
-function buildWords() {
+async function buildWords() {
     document.getElementById('ready').innerHTML = '';
 
     var lettersText = document.getElementById('letters').value;
@@ -13,34 +13,37 @@ function buildWords() {
     lettersCount = letters.length;
 
     for (let len = MIN_LENGTH; len <= lettersCount; len++) { // length of words 4..9
-        createWordsOfGivenLength(len, letters, mustLetter);
+        await createWordsOfGivenLength(len, letters, mustLetter);
     }
     document.getElementById('ready').innerHTML = 'Valmis!';
 }
 
-function createWordsOfGivenLength(wordLength, letters, mustLetter) {
+async function createWordsOfGivenLength(wordLength, letters, mustLetter) {
     let wordLettersArr = new Array(wordLength);
     let usedLetterIndexes = new Set();
-    addLetterAtIndex(wordLettersArr, 0, wordLength, letters, usedLetterIndexes, mustLetter);
+    await addLetterAtIndex(wordLettersArr, 0, wordLength, letters, usedLetterIndexes, mustLetter);
 }
 
-function addLetterAtIndex(wordLettersArr, letterIndex, wordLength, letters, usedLetterIndexes, mustLetter) {
+async function addLetterAtIndex(wordLettersArr, letterIndex, wordLength, letters, usedLetterIndexes, mustLetter) {
     if (letterIndex < wordLength) {
         for (let index = 0; index < lettersCount; index++) {
             if (!usedLetterIndexes.has(index)) {
                 usedLetterIndexes.add(index);
                 wordLettersArr[letterIndex] = letters[index];
                 if (letterIndex == wordLength - 1 && wordLettersArr.includes(mustLetter)) {
-                    printWord(wordLettersArr, wordLength, mustLetter);
+                    await printWord(wordLettersArr);
                 }
-                addLetterAtIndex(wordLettersArr, letterIndex + 1, wordLength, letters, usedLetterIndexes, mustLetter);
+                await addLetterAtIndex(wordLettersArr, letterIndex + 1, wordLength, letters, usedLetterIndexes, mustLetter);
                 usedLetterIndexes.delete(index);
             }
         }
     }
 }
 
-function printWord(wordLettersArr, wordLength, mustLetter) {
-    let word = wordLettersArr.join('');
-    document.getElementById('words').value += word + '\n';
+async function printWord(wordLettersArr) {
+    return new Promise((resolve) => {
+        let word = wordLettersArr.join('');
+        document.getElementById('words').value += word + '\n';
+        resolve();
+    });
 }
